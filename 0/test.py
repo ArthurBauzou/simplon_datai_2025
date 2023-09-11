01b3edf517c351b151e14673e5c1af86dc3588a1
arthurbauzou <arthurbauzou@gmail.com>
2023-09-11 14:37:52 +0200

    yaducode

--- 0/test.py
+++ 0/test.py
@@ -1,1 +1,30 @@
-salut python c’est pas encore du code python
+import random
+
+first_names = [
+    "pasclal",
+    "paul",
+    "violette",
+    "claude",
+    "cheryl",
+    "tony",
+    "hedwige",
+    "terry"
+]
+
+last_names = [
+    "charpentier",
+    "swift",
+    "wick",
+    "mclane",
+    "bertignac",
+    "parker",
+    "tsonga",
+    "lee"
+]
+
+def randomname() :
+    new_first_name = random.choice(first_names)
+    new_last_name = random.choice(last_names)
+    return new_first_name + new_last_name
+
+
