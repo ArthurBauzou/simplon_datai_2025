c7725524d9c13d6d98e0b23686ddb5f0199f293c
arthurbauzou <arthurbauzou@gmail.com>
2023-09-11 16:24:50 +0200

    nom capitalisé

--- 0/test.py
+++ 0/test.py
@@ -1,30 +1,40 @@
 import random
 
-first_names = [
-    "pasclal",
-    "paul",
-    "violette",
-    "claude",
-    "cheryl",
-    "tony",
-    "hedwige",
-    "terry"
-]
-
-last_names = [
-    "charpentier",
-    "swift",
-    "wick",
-    "mclane",
-    "bertignac",
-    "parker",
-    "tsonga",
-    "lee"
-]
-
 def randomname() :
+    first_names = [
+        "pascal",
+        "paul",
+        "violette",
+        "claude",
+        "cheryl",
+        "tony",
+        "hedwige",
+        "terry"
+    ]
+    last_names = [
+        "charpentier",
+        "swift",
+        "wick",
+        "mclane",
+        "bertignac",
+        "parker",
+        "tsonga",
+        "lee"
+    ]
     new_first_name = random.choice(first_names)
     new_last_name = random.choice(last_names)
-    return new_first_name + new_last_name
+    return new_first_name + " " + new_last_name
 
+def majusculator(str) :
+    words = str.split()
+    capitalised_name = ""
+    for word in words:
+        capital_letter = word[0].upper()
+        rest = word.split(word[0],1)[1]
+        capitalised_name += capital_letter + rest + ' '
 
+    return capitalised_name
+
+print(majusculator(randomname()))
+
+# print(randomname())
