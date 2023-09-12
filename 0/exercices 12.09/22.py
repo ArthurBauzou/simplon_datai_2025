91c9e4e92b60348f7007dd70fa44df898d0928ae
arthurbauzou <arthurbauzou@gmail.com>
2023-09-12 15:33:11 +0200

    exos 21 et 22

---
+++ 0/exercices 12.09/22.py
@@ -0,0 +1,17 @@
+weird:float = 0.1 + 0.2
+
+print(weird)
+
+messages = [
+    "Ce résultat étrange est généré par la façon dont le binaire fonctionne.",
+    "De la même façon que 1/3 est difficile à noter pour nous qui utilisons un système en base 10,"
+    "pour un ordinateur il est inconfortable de manipuler certaines valeurs car il devra toujours les arrondir?",
+    "Bien sur les ordinateurs sont bien faits et cet artefact ne pose pas de soucis à long terme, c’est juste un",
+    "artefact de notation."
+]
+    
+verif = weird + 0.7
+print(verif)
+
+weird_display = round(weird, 1)
+print(weird_display)
