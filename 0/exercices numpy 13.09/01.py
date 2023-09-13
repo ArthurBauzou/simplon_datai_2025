091d22b3f28e3d99637e92e711d3c7dcd1bd8710
arthurbauzou <arthurbauzou@gmail.com>
2023-09-13 09:42:12 +0200

    debut numpy

---
+++ 0/exercices numpy 13.09/01.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+tabl = np.arange(1,11)
+print(tabl)
---
+++ 0/exercices numpy 13.09/03.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+tabl = np.array([1, 3, 5, 7, 9])
+
+def my_sum(arr:np.array) :
+    return np.sum(tabl)
+
+print(my_sum(tabl))
